2f7e5419a69184c34f0130541183c9765bce2e22
deepa-1211-nok <68528245+deepa-1211-nok@users.noreply.github.com>
2020-09-11 12:59:14 +0900

    Add files via upload

---
+++ psmg.py
@@ -0,0 +1,32 @@
+def Registration():
+	nrg = []
+	print('タイトルを入力してください。')
+	title = input()
+	print('IDを入力してください。')
+	ID = input()
+	print('パスワードを入力してください')
+	pw = input()
+	print(title,ID,pw)
+	list = [title,ID,pw]
+	nrg.append(list)
+	print(nrg)
+
+
+
+def main():
+	Registration()
+	# print('Menu')
+	# print('List(ls)')
+	# print('Serch(sc)')
+	# print('Registration(rg)')
+	# print('change(cg)')
+
+if __name__ == '__main__':
+	main()
+
+
+
+# if command = 'rg':
+	# return Registration
+
+	
